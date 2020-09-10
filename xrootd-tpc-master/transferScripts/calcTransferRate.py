36f70ae460b71bbe127dd7819a9fd0123c2348aa
aaarora <aaarora@ucsd.edu>
2020-09-10 15:40:04 -0700

    update

--- xrootd-tpc-master/transferScripts/calcTransferRate.py
+++ xrootd-tpc-master/transferScripts/calcTransferRate.py
@@ -73,7 +73,8 @@ def main():
         for (destName, destIP) in conf.items():
             if sourceName is not destName:
                 rate = doTransfer(sourceIP, destIP, 1)
-                rateDict.update({"{0}~{1}~{2}~{3}".format(sourceName,sourceIP,destName,destIP) : rate})
+                if rate is not 0:
+                    rateDict.update({"{0}~{1}~{2}~{3}".format(sourceName,sourceIP,destName,destIP) : rate})
 
     with open('/home/rates.json','w') as out:
         json.dump(rateDict,out)
